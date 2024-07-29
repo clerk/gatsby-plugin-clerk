4d94e69af1fec3d09520b4c21ff7a9cd28f73107
wobsoriano <sorianorobertc@gmail.com>
2024-07-29 12:47:39 -0700

    chore: Clean contents

--- plugin/gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-import { WrapRootElement } from './src/provider'
-
-export const wrapRootElement = ({ element }, pluginOptions) => {
-    const { plugins, ...clerkOptions } = pluginOptions
-
-    return <WrapRootElement element={element} options={clerkOptions} />
-}
