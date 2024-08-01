c29edacaf17802c410d9fcd0908d8ef4909283b2
wobsoriano <sorianorobertc@gmail.com>
2024-08-01 06:05:35 -0700

    Clean up tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,6 @@
-import type { Options } from 'tsup';
 import { defineConfig } from 'tsup';
 
 import { name, version } from './package.json';
-
-// Copied from https://github.com/clerk/javascript/blob/a6620ae2e8a871639ee8b00e3677b63b179663a8/scripts/utils.ts#L3
-const runAfterLast =
-  (commands: Array<string | false>) =>
-  (...configs: Options[]) => {
-    const [last, ...rest] = configs.reverse();
-    return [...rest.reverse(), { ...last, onSuccess: [last.onSuccess, ...commands].filter(Boolean).join(' && ') }];
-  };
 
 const clerkJsVersion = '5';
 
@@ -17,7 +8,7 @@ export default defineConfig(overrideOptions => {
   const isWatch = !!overrideOptions.watch;
   const shouldPublish = !!overrideOptions.env?.publish;
 
-  const options: Options = {
+  return {
     format: 'cjs',
     outDir: './dist',
     entry: ['./src/**/*.{ts,tsx,js,jsx}'],
@@ -32,7 +23,6 @@ export default defineConfig(overrideOptions => {
       JS_PACKAGE_VERSION: `"${clerkJsVersion}"`,
       __DEV__: `${isWatch}`,
     },
+    onSuccess: `npm run build:declarations ${shouldPublish ? '&& npm run publish:local' : ''}`
   };
-
-  return runAfterLast(['npm run build:declarations', shouldPublish && 'npm run publish:local'])(options);
 });
